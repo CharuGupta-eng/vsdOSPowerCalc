79428bedbcf2a69e902a6fdcbe96d195eedd1984
CharuGupta-eng <66687579+CharuGupta-eng@users.noreply.github.com>
2020-07-06 05:19:42 +0530

    Add files via upload

--- Leakage.py
+++ Leakage.py
@@ -1,13 +1,18 @@
-print("You have to remove some lines from netlist file that lines are remove analysis(.dc or .tran) and .control, run, end, endc statemant,and most important all input and clock pulses\n\n")
+import os
+
+print("You have to remove some lines from netlist file that lines are remove analysis(.dc or .tran), supply voltage line and .control, run, end, endc statemant,and most important all input and clock pulses\n\n")
 s= (input("enter file name with .cir or .txt"))
-Vtstp = (input("enter name of voltage source having 0V that you placed between Supply Voltage and pmos\n\n"))
-k= (input("Enter name of node of supply voltage source (which you have given and used in place of all supply voltages)\n"))
+V_value = float(input("enter value of Supply Voltage in V"))
+vd = (input("Enter name of node of supply voltage source (which you have given and used in place of all supply voltages\n"))
+#Vsup = (input("enter name of supply voltage source\n\n"))
 shakes = open(s, "a")
+shakes.write("VSUP netnet 0 DC {}\n" .format(V_value))
+shakes.write("Vtstp netnet {} DC 0" .format(vd))
+#Vtstp = (input("enter name of voltage source having 0V that you placed between Supply Voltage and pmos\n\n"))
+#k= (input("Enter name of node of supply voltage source (which you have given and used in place of all supply voltages)\n"))
 
-shakes.write("\n.op\n.control\nrun\nprint I({})*V({})\n.endc\n.end".format(Vtstp,k))
+shakes.write("\n.op\n.control\nrun\nprint I(Vtstp)*V({})\n.endc\n.end".format(vd))
+shakes.close()
+os.system('ngspice '+s)
 
-# exec --ngspice(s)
-print("I made required changes in your file now you can run in ngsice\n\n\n")
-
-print("You got leakage power value")
 
--- POW.py
+++ POW.py
@@ -1,22 +1,31 @@
-print("You have to remove some lines from netlist file that lines are remove analysis (.tran or .dc) and .control, run, end, endc statemant\n")
+import os
+print("You have to remove some lines from netlist file that lines are remove analysis (.tran or .dc), supply voltage line and .control, run, end, endc statemant\n")
 s= (input("enter file name with .cir or .txt"))
 C = 10
 R = 100
 T = float(input("Enter the time period in seconds of clock in synchronous circuit or in asynchronous circuit take time period of input having minimum time period"))
 J = T*5
 if R*C< T:
-    print("MULTIPLICATION OF RESISTOR AND CAPACITOR of my power measure circuit is smaller THAN YOUR ENTERED TIME PERIOD")
+    print("MULTIPLcd cdICATION OF RESISTOR AND CAPACITOR of my power measure circuit is smaller THAN YOUR ENTERED TIME PERIOD")
     print("Now you have to choose value of capacitor and resistor acccordingly that product of r and c must be greater than your time perid")
     C = float(input("enter value of capacitor in nF"))
     R = float(input("enter value of resistor in Kohm"))
-VDD = float(input("enter value of Supply Voltage in V"))
-BETA = (VDD*C)/(T*(10**9))
-Vtstp = (input("enter name of voltage source having 0V that you placed between Supply Voltage and pmos\n\n"))
+V_value = float(input("enter value of Supply Voltage in V"))
+vd = (input("Enter name of node of supply voltage source (which you have given and used in place of all supply voltages\n"))
+print("You got power curve and values, COPIES ALL THE OUTPUT VALUES AND MAKE .CSV FILE TO GET ONE VALUE BY runing avg.py")
+BETA = (V_value*C)/(T*(10**9))
+#Vsup = (input("enter name of supply voltage source\n\n"))
 shakes = open(s, "a")
-shakes.write("\nfp 0 POWR {} {}\n" .format(Vtstp, BETA))
-shakes.write("C1 POWR 0 {}n\n".format(C))
-shakes.write ("R1 POWR 0 {}K\n".format(R))
+shakes.write("VSUP netnet 0 DC {}\n" .format(V_value))
+shakes.write("Vtstp netnet {} DC 0" .format(vd))
+shakes.write("\nfp 0 POWR vtstp {}\n" .format(BETA))
+shakes.write("Cp POWR 0 {}n\n".format(C))
+shakes.write ("Rp POWR 0 {}K\n".format(R))
 shakes.write(".tran 1e-0 {} UIC\n.control\nrun\nPLOT V(POWR)\nPRINT V(POWR)\n.endc\n.end".format(J))
+shakes.close()
+
+os.system('ngspice '+s)
 print("I made required changes in your file now you can run in ngsice\n\n\n")
 
-print("You got power curve and values, to find one value run avg.py")
+
+
