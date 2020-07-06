2b374121820973e2e24c0c9b77bca572c6332037
CharuGupta-eng <66687579+CharuGupta-eng@users.noreply.github.com>
2020-07-06 17:41:37 +0530

    Add files via upload

--- avg.py
+++ avg.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
-print("cpoy the output you get in table format and make .csv file in excel\n\n")
+print("copy the output you get in table format and make .csv file in excel\n\n")
 print("name each coloumn as INDEX, TIME and POWER\n\n\n\n")
-s = input("Enter the file name with extension .csv")
+s = input("Enter the file name with extension .csv...")
 
 file = pd.read_csv(s)
-t = float(input("\nEnter the time period in seconds"))
+#print(file)
+t = float(input("\nEnter the time period in seconds..."))
 FILE1 = file[file["TIME"]==t]
 FILE2 = file[file["TIME"]==0]
 m = FILE1["POWER"].max()
