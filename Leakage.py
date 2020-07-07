75d6d4f851741e743c615651564a99fbeb9bbc4b
CharuGupta-eng <66687579+CharuGupta-eng@users.noreply.github.com>
2020-07-07 17:42:38 +0530

    Add files via upload

--- Leakage.py
+++ Leakage.py
@@ -1,11 +1,32 @@
 import os
+import re
 
-print("You have to remove some lines from netlist file that lines are remove analysis(.dc or .tran), supply voltage line and .control, run, end, endc statemant,and most important all input and clock pulses\n\n")
-s= (input("enter file name with .cir or .txt"))
-V_value = float(input("enter value of Supply Voltage in V"))
-vd = (input("Enter name of node of supply voltage source (which you have given and used in place of all supply voltages\n"))
+print("You have to remove all NON CONSTANT (PULSE) input and clock pulses\n\n")
+s= (input("enter file name with .cir or .txt (e.g. TT.cir):"))
+V_value = float(input("enter value of Supply Voltage in V (e.g. 1.8): "))
+vd = (input("Enter name of node of supply voltage source (which you have given and used in place of all supply voltages)(e.g. VDD) \n"))
+vs = (input("Enter the name of the voltage supply (e.g. V_V20): "))
 #Vsup = (input("enter name of supply voltage source\n\n"))
-shakes = open(s, "a")
+
+myinp=open(s,"r")
+shakes = open("poweranalysis.cir", "w")
+done=False
+
+for line in myinp.readlines():
+    #if re.match(r'^\.tran',line):
+        #line=line.rstrip()+" UIC\n"
+       # line=""
+    if re.match(r'\.control',line):
+        done=True
+    if re.match(r'^'+vs,line):
+        #if re.match(r'(\d+\.?\d*)V?d?s?\s*$',line):
+        #    V_value=re.match(r'(\d+\.?\d*)V?d?s?\s*$').group(1)
+        line=''
+    if not done and not re.match(r'^(V_V\d+\s+N51925|\.endc|.end|.tran)',line) :
+        shakes.write(line)
+
+myinp.close()
+
 shakes.write("VSUP netnet 0 DC {}\n" .format(V_value))
 shakes.write("Vtstp netnet {} DC 0" .format(vd))
 #Vtstp = (input("enter name of voltage source having 0V that you placed between Supply Voltage and pmos\n\n"))
@@ -13,6 +34,6 @@ shakes.write("Vtstp netnet {} DC 0" .format(vd))
 
 shakes.write("\n.op\n.control\nrun\nprint I(Vtstp)*V({})\n.endc\n.end".format(vd))
 shakes.close()
-os.system('ngspice '+s)
+os.system('ngspice poweranalysis.cir')
 
 
